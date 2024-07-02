5865c9353d2d6290421d0203487733502da4a93f
mangmuse <mangmangmuse@gmail.com>
2024-07-02 19:49:05 +0900

    Style: pokemonDetailPage

--- src/app/(root)/[pokemonId]/page.tsx
+++ src/app/(root)/[pokemonId]/page.tsx
@@ -22,31 +22,65 @@ export default async function PokemonDetailPage({
   const pokemon = await getPokemonData(pokemonId);
 
   return (
-    <div className="text-white">
-      <section className="">
-        <h3 className="text-3xl ">이건 특성</h3>
-        {pokemon.abilities.map((abil) => abil.ability.korean_name)}
-        <div>이건 키 {pokemon.height}</div>
-        <div>이건 도감번호 {pokemon.id}</div>
-        <div>이건 이름 {pokemon.korean_name}</div>
-        <div className="flex gap-8">
-          <h3 className="text-3xl">이건 기술</h3>
-          <span> {pokemon.moves.map((move) => move.move.korean_name)}</span>
-        </div>
-        <Image
-          src={pokemon.sprites.front_default}
-          alt={pokemon.id.toString()}
-          width={300}
-          height={300}
-        ></Image>
-        <div>
-          <h3 className="text-3xl">이건타입</h3>
-          {pokemon.types.map((type, idx) => (
-            <span key={idx}>{type.type.korean_name}</span>
-          ))}
-        </div>
-        <div>{pokemon.weight}</div>
-        <Link href="/">뒤로가기</Link>
+    <div className="h-full flex justify-center items-center  ">
+      <section className="max-w-screen-sm bg-gray-100 rounded-xl">
+        <header className=" p-5 flex flex-col items-center bg-gray-200 rounded-xl">
+          <h3 className="text-2xl font-semibold"> {pokemon.korean_name}</h3>
+          <span>No. {pokemon.id}</span>
+        </header>
+        <article className="flex flex-col items-center p-4 ">
+          <Image
+            src={pokemon.sprites.front_default}
+            alt={pokemon.id.toString()}
+            width={150}
+            height={150}
+          ></Image>
+          <h3>이름: {pokemon.korean_name}</h3>
+          <p>
+            키: {pokemon.height} m 무게: {pokemon.weight} kg
+          </p>
+          <div className="flex gap-2">
+            <div className="flex items-center gap-2">
+              <span className="font-semibold">타입: </span>
+              <ul className="flex items-center gap-1">
+                {pokemon.types.map((type, idx) => (
+                  <li
+                    className="text-sm rounded-lg bg-red-400 px-2 leading-5"
+                    key={idx}
+                  >
+                    {type.type.korean_name}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="flex items-center gap-2 ">
+              <span className="font-semibold ">특성: </span>
+              <ul className="flex  items-center gap-1">
+                {pokemon.abilities.map((abil) => (
+                  <p
+                    key={abil.ability.korean_name}
+                    className="text-sm rounded-lg bg-green-300 px-2"
+                  >
+                    {abil.ability.korean_name}
+                  </p>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <ul className="flex flex-wrap gap-1 overflow-auto mt-2">
+            {pokemon.moves.map((move) => (
+              <span className="text-sm" key={move.move.korean_name}>
+                {move.move.korean_name}
+              </span>
+            ))}
+          </ul>
+          <Link
+            className="text-white bg-blue-500 px-3 py-1 rounded-md mt-2"
+            href="/"
+          >
+            목록으로
+          </Link>
+        </article>
       </section>
     </div>
   );
